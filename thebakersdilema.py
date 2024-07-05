7eb640c492299b9af5976e06fb2987df84e2018c
ksol17 <karla.pauta001@gmail.com>
2024-07-05 09:45:44 -0400

    commit

---
+++ thebakersdilema.py
@@ -0,0 +1,6 @@
+# If one cake requires 250 grams of flour and you have 2.5 kilograms of flour, how many cakes can you make using the arithmetic operators?
+
+flour_per_cake = 250
+total_flour = 2.5 * 1000
+number_of_cakes = total_flour // flour_per_cake
+print(number_of_cakes)
